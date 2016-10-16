e31cfa86182de23c9008b32c079e1842784d1865
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-10-16 12:11:36 -0700

    Specify bootstrap key

--- freebsd-init/local.sh
+++ freebsd-init/local.sh
@@ -14,6 +14,10 @@ case $i in
       bootstrap_user="${i#*=}"
       shift # past argument=value
     ;;
+    --bootstrap-id-file=*)
+      bootstrap_id_file="${i#*=}"
+      shift # past argument=value
+    ;;
     --primary-user=*)
       primary_user="${i#*=}"
       shift # past argument=value
@@ -33,11 +37,10 @@ case $i in
 esac
 done
 
-pub_key_file="${pub_key_file:-$HOME/.ssh/id_rsa.pub}"
-
 printf "%-16s\t%s\n" \
   "Host" "$host" \
   "Bootstrap User" "$bootstrap_user" \
+  "Bootstrap Id File" "$bootstrap_id_file" \
   "Primary User" "$primary_user" \
   "Primary Name" "$primary_name" \
   "Pub key file" "$pub_key_file"
@@ -48,18 +51,19 @@ printf "%s\n" "------------------"
 
 tag="[main]"
 
+bootstrap_flags="-i $bootstrap_id_file"
 bootstrap_target="${bootstrap_user}@${host}"
 primary_target="${primary_user}@${host}"
 
 printf "$tag Copying files\n"
-scp bootstrap.sh setup_home.sh "$bootstrap_target:~"
-scp "$pub_key_file" "$bootstrap_target:~/authorized_keys"
+scp "$bootstrap_flags" bootstrap.sh setup_home.sh "$bootstrap_target:~"
+scp "$bootstrap_flags" "$pub_key_file" "$bootstrap_target:~/authorized_keys"
 
 printf "$tag Running bootstrap.sh\n"
-ssh "$bootstrap_target" "sudo /bin/sh ./bootstrap.sh $primary_user '$primary_name'"
+ssh "$bootstrap_flags" "$bootstrap_target" "sudo /bin/sh ./bootstrap.sh $primary_user '$primary_name'"
 
 printf "$tag Running setup_home.sh\n"
-ssh "$bootstrap_target" "sudo -u $primary_user /bin/sh /home/$bootstrap_user/setup_home.sh $bootstrap_user"
+ssh "$bootstrap_flags" "$bootstrap_target" "sudo -u $primary_user /bin/sh /home/$bootstrap_user/setup_home.sh $bootstrap_user"
 
 printf "$tag Running setup_env.sh\n"
 ssh "$bootstrap_target" "sudo -u $primary_user /bin/sh" < setup_env.sh
