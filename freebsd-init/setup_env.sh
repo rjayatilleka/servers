7f72c9dbce56c9cdad9c828bed69a2cbd2c8e606
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-10-15 19:17:54 -0700

    Setup env done, more robustness

--- freebsd-init/setup_env.sh
+++ freebsd-init/setup_env.sh
@@ -8,4 +8,28 @@ tag="[setup_env]"
 
 printf "$tag Starting: USER=%s ...\n" "$USER"
 
+# Install packages
+printf "$tag Installing packages...\n"
+sudo pkg install -y git neovim fzf
+
+# Setting up config home
+printf "$tag Setting up .config/...\n"
+cd
+mkdir -p .config
+
+# Cloning bashrc
+printf "$tag Cloning bashrc...\n"
+cd ~/.config
+[ -d bashrc ] || git clone https://github.com/rjayatilleka/bashrc
+cd
+printf "echo \$HOSTNAME\nsource $HOME/.bashrc\n" > .bash_profile
+printf "source $HOME/.config/bashrc/main.bash\n" > .bashrc
+
+# Cloning vimrc
+printf "$tag Cloning vimrc...\n"
+cd ~/.config
+[ -d nvim ] || git clone https://github.com/rjayatilleka/nvim-config nvim
+cd nvim
+sed -i'' -e "s/^Module 'plugins'\$/\" \\0/" init.vim
+
 printf "$tag Done setting up primary env.\n"
--- freebsd-init/setup_home.sh
+++ freebsd-init/setup_home.sh
@@ -13,11 +13,16 @@ printf "$tag Starting: bootstrap_user=%s USER=%s ...\n" "$bootstrap_user" "$USER
 # Clean home directory
 printf "$tag Cleaning home directory...\n"
 cd
-rm -rf * .[!.]*
+rm -rf .cshrc	.login_conf .mailrc	.rhosts .login .mail_aliases .profile .shrc
 
 # Setup ssh
 printf "$tag Setting up ssh...\n"
-mkdir .ssh
-cp "/home/$bootstrap_user/authorized_keys" .ssh/authorized_keys
+mkdir -p .ssh
+src_ak="/home/$bootstrap_user/authorized_keys"
+dest_ak="$HOME/.ssh/authorized_keys"
+if ! ([ -f "$dest_ak" ] || cmp --silent "$src_ak" "$dest_ak"); then
+  printf "$tag Adding authorized key...\n"
+  cat "$src_ak" >> "$dest_ak"
+fi
 
 printf "$tag Done setting up primary home.\n"
